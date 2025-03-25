6098f65e39599cafa61097c7bc167705d7fecd7f
AngelloPaperplane <dev@paperplane.co>
2025-03-24 23:24:03 -0500

    [publish] repository

--- src/GenerateInfoFiles.java
+++ src/GenerateInfoFiles.java
@@ -1,6 +1,7 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
+
 /**
 * Clase para generar archivos de prueba pseudoaleatorios para el proyecto.
 * @author Angelo Hernández
@@ -73,3 +74,4 @@ public class GenerateInfoFiles {
        }
    }
 }
+
